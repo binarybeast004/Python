8bd8251a47e16c045ba53224d6f9cb565a0c5fd2
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2022-06-01 10:03:01 +0600

    Update sort_list.py

--- sort_list.py
+++ sort_list.py
@@ -1,3 +1,8 @@
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort()
 print(thislist)
+
+s = ["orange","mango","kiwi","pineapple","banana"]
+s.sort(reverse = True)
+print(s)
+
